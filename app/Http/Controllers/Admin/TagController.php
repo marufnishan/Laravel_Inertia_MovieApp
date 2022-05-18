41aeb28c8f52e3194343c10ec519f7d664a5d281
marufnishan <nasim15-2328@diu.edu.bd>
2022-05-18 11:49:38 +0600

    Tags Create Added

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request ;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class TagController extends Controller
 {
@@ -16,5 +19,15 @@ class TagController extends Controller
     public function create()
     {
         return Inertia::render('Tags/create');
+    }
+
+    public function store()
+    {
+        Tag::create([
+            'tag_name' => Request::input('tagName'),
+            'slug' => Str::slug(Request::input('tagName'))
+        ]);
+
+        return Redirect::route('admin.tags.index');
     }
 }
